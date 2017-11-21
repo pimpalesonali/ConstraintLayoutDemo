0c20bafaefe88a07840864e0fa06970e9d2d491f
pimpalesonali <pimpale.sonali94@gmail.com>
2017-11-21 10:51:45 +0530

    viewanimations

---
+++ viewanimations/src/main/java/com/android/sonali/viewanimations/MainActivity.java
@@ -0,0 +1,25 @@
+package com.android.sonali.viewanimations;
+
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void onAnimate(View view)
+    {
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.rotate);
+        findViewById(R.id.imageView).startAnimation(animation);
+
+    }
+}
